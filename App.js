73c1ffbe0c0eacb5364cbb2fb5b51f266057a0e1
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-18 10:56:23 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import {Text,} from 'react-native';
+
+import Home from './src/screens/containers/home';
+import Header from './src/sections/components/header';
+import SuggestionList from './src/videos/containers/suggestion-list';
+
+type Props = {};
+export default class App extends Component<Props> {
+    render() {
+        return (
+            <Home>
+                <Header/>
+                <Text>buscador</Text>
+                <Text>categorías</Text>
+                <Text>sugerencias</Text>
+                <SuggestionList />
+            </Home>
+        );
+    }
+}
