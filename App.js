632f4cb45d105f79eddea5588854d460d5b2bcbb
Rafael Bastos <rafael.bastos@softec.com.py>
2018-10-18 13:32:24 -0300

    integracion con yts.am/api

--- App.js
+++ App.js
@@ -4,9 +4,14 @@ import {Text,} from 'react-native';
 import Home from './src/screens/containers/home';
 import Header from './src/sections/components/header';
 import SuggestionList from './src/videos/containers/suggestion-list';
+import API from './utils/api';
 
 type Props = {};
 export default class App extends Component<Props> {
+    async componentDidMount() {
+        const movies = await API.getSuggestion(10);
+        console.log(movies);
+    }
     render() {
         return (
             <Home>
